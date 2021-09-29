8b0a5addebb220a64503ce2723f48e5be4bfcccc
Isabel Suchanek <isabel@hashicorp.com>
2021-09-29 11:31:13 -0700

    add hcl to var-file completion

--- cli/commands.go
+++ cli/commands.go
@@ -237,8 +237,7 @@ func (c *baseCommand) flagSet(bit flagSetBit, f func(*flag.Sets)) *flag.Sets {
 				Default: make([]string, 0),
 				Usage: `Specifies the path to a variable override file. This can be provided 
 				multiple times on a single command to result in a list of files.`,
-				// TODO: this is what's in nomad. Do we want to modify this so it accepts .hcl?
-				Completion: complete.PredictFiles("*.var"),
+				Completion: complete.PredictOr(complete.PredictFiles("*.var"), complete.PredictFiles("*.hcl")),
 			},
 			Shorthand: "f",
 		})
