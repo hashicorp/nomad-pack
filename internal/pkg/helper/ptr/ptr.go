6e5bc4de4b7ee55ee753fbd8ec502fd8da8aa7b3
James Rasell <jrasell@hashicorp.com>
2021-10-20 12:28:45 +0200

    makefile: add sdk isolation check and fix failure.
    This adds a check to the makefile to ensure the sdk is isolated
    from the rest of the nomad-pack code. As a result this also moves
    the pointer helper into the sdk, as it is only used within the
    sdk.

--- internal/pkg/helper/ptr/ptr.go
+++
@@ -1,4 +0,0 @@
-package ptr
-
-// Bool returns a pointer to the input boolean value.
-func Bool(i bool) *bool { return &i }
---
+++ sdk/helper/ptr.go
@@ -0,0 +1,4 @@
+package helper
+
+// BoolToPtr returns a pointer to the input boolean value.
+func BoolToPtr(i bool) *bool { return &i }
