09f8c40bbfa86261b4da5e3c545ec1334b3c421c
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-02-01 10:38:44 -0500

    Fix flag tests

--- internal/pkg/flag/set_test.go
+++ internal/pkg/flag/set_test.go
@@ -7,29 +7,67 @@ import (
 )
 
 func TestSets(t *testing.T) {
-	require := require.New(t)
-
 	var valA, valB int
 	sets := NewSets()
 	{
-		set := sets.NewSet("A")
-		set.IntVar(&IntVar{
-			Name:   "a",
-			Target: &valA,
+		set := sets.NewSet("setA")
+		set.IntVarP(&IntVarP{
+			IntVar: &IntVar{
+				Name:   "alpha",
+				Target: &valA,
+			},
+			Shorthand: "a",
 		})
 	}
 
 	{
-		set := sets.NewSet("B")
-		set.IntVar(&IntVar{
-			Name:   "b",
-			Target: &valB,
+		set := sets.NewSet("setB")
+		set.IntVarP(&IntVarP{
+			IntVar: &IntVar{
+				Name:   "beta",
+				Target: &valB,
+			},
+			Shorthand: "b",
 		})
 	}
 
-	err := sets.Parse([]string{"-b", "42", "-a", "21"})
-	require.NoError(err)
-
-	require.Equal(int(21), valA)
-	require.Equal(int(42), valB)
+	testCases := []struct {
+		Name        string
+		Args        []string
+		expectError bool
+	}{
+		{
+			Name: "all small flags",
+			Args: []string{"-b", "42", "-a", "21"},
+		},
+		{
+			Name: "positional argument in the middle",
+			Args: []string{"-b", "42", "something", "-a", "21"},
+		},
+		{
+			Name: "mixed flag types after positionals",
+			Args: []string{"-b", "42", "something", "--alpha", "21"},
+		},
+		{
+			Name: "posix-style long flags",
+			Args: []string{"--beta", "42", "something", "--alpha", "21"},
+		},
+		{
+			Name:        "missing value",
+			Args:        []string{"-d", "42", "-a", "21"},
+			expectError: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			err := sets.Parse(tc.Args)
+			if tc.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, int(21), valA)
+				require.Equal(t, int(42), valB)
+			}
+		})
+	}
 }
