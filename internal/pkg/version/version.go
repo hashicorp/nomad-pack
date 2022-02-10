568ac5e42bc41172a1fa3c8b18af2f42b9e341ff
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-02-10 12:05:04 -0500

    post v0.0.1-techpreview2 release changes (#206)
    Prepare CHANGELOG.md and internal/pkg/version/version.go ready for techpreview3

--- internal/pkg/version/version.go
+++ internal/pkg/version/version.go
@@ -21,7 +21,7 @@ var (
 	// Prerelease designates whether the current version is within a prerelease
 	// phase. Typically, this will be "dev" to signify a development cycle or a
 	// release candidate phase such as alpha, beta, rc1, or such.
-	Prerelease = "techpreview2"
+	Prerelease = "techpreview3"
 
 	// Metadata allows us to provide additional metadata information to the
 	// version identifier. This is typically used to identify enterprise builds
