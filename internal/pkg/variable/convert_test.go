16683372fbcca2422429595e8743b375fec3e18a
Luiz Aoqui <luiz@hashicorp.com>
2022-07-21 14:23:05 -0400

    minor test updates

--- internal/pkg/variable/convert_test.go
+++ internal/pkg/variable/convert_test.go
@@ -4,70 +4,80 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func TestConvertCtyToInterface(t *testing.T) {
 
 	// test basic type
-	tables := []struct {
-		val cty.Value
-		t   reflect.Kind
+	testCases := []struct {
+		name string
+		val  cty.Value
+		t    reflect.Kind
 	}{
-		{cty.BoolVal(true), reflect.Bool},
-		{cty.StringVal("test"), reflect.String},
-		{cty.NumberIntVal(1), reflect.Int},
-		{cty.MapVal(map[string]cty.Value{"test": cty.BoolVal(true)}), reflect.Map},
+		{"bool", cty.BoolVal(true), reflect.Bool},
+		{"string", cty.StringVal("test"), reflect.String},
+		{"number", cty.NumberIntVal(1), reflect.Int},
+		{"map", cty.MapVal(map[string]cty.Value{"test": cty.BoolVal(true)}), reflect.Map},
 	}
 
-	for _, table := range tables {
-		res, err := convertCtyToInterface(table.val)
-		resType := reflect.TypeOf(res).Kind()
-		if resType != table.t || err != nil {
-			t.Errorf("Error while converting type %v", table.t)
-		}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := convertCtyToInterface(tc.val)
+			require.NoError(t, err)
 
+			resType := reflect.TypeOf(res).Kind()
+			require.Equal(t, tc.t, resType)
+		})
 	}
 
 	// test list of list
-	testListOfList := cty.ListVal([]cty.Value{
-		cty.ListVal([]cty.Value{
-			cty.BoolVal(true),
-		}),
+	t.Run("lists of lists", func(t *testing.T) {
+		testListOfList := cty.ListVal([]cty.Value{
+			cty.ListVal([]cty.Value{
+				cty.BoolVal(true),
+			}),
+		})
+
+		resListOfList, err := convertCtyToInterface(testListOfList)
+		require.NoError(t, err)
+
+		tempList, ok := resListOfList.([]interface{})
+		require.True(t, ok)
+
+		_, ok = tempList[0].([]interface{})
+		require.True(t, ok)
 	})
-
-	resListOfList, err := convertCtyToInterface(testListOfList)
-	tempList, checkListOfList1 := resListOfList.([]interface{})
-	_, checkListOfList2 := tempList[0].([]interface{})
-
-	if err != nil || checkListOfList1 != true || checkListOfList2 != true {
-		t.Errorf("Error while converting type list of list")
-	}
 
 	// test list of maps
-	testListOfMaps := cty.ListVal([]cty.Value{
-		cty.MapVal(map[string]cty.Value{
-			"test": cty.BoolVal(true),
-		}),
-	})
-	resListOfMaps, err := convertCtyToInterface(testListOfMaps)
+	t.Run("list of maps", func(t *testing.T) {
+		testListOfMaps := cty.ListVal([]cty.Value{
+			cty.MapVal(map[string]cty.Value{
+				"test": cty.BoolVal(true),
+			}),
+		})
 
-	_, checkListOfMaps := resListOfMaps.([]map[string]interface{})
-	if err != nil || checkListOfMaps != true {
-		t.Errorf("Error while converting type list of maps")
-	}
+		resListOfMaps, err := convertCtyToInterface(testListOfMaps)
+		require.NoError(t, err)
+
+		_, ok := resListOfMaps.([]map[string]interface{})
+		require.True(t, ok)
+	})
 
 	// test map of maps
-	testMapOfMaps := cty.MapVal(map[string]cty.Value{
-		"test": cty.MapVal(map[string]cty.Value{"test": cty.BoolVal(true)}),
+	t.Run("map of maps", func(t *testing.T) {
+		testMapOfMaps := cty.MapVal(map[string]cty.Value{
+			"test": cty.MapVal(map[string]cty.Value{"test": cty.BoolVal(true)}),
+		})
+
+		restMapOfMaps, err := convertCtyToInterface(testMapOfMaps)
+		require.NoError(t, err)
+
+		tempMapOfMaps, ok := restMapOfMaps.(map[string]interface{})
+		require.True(t, ok)
+
+		_, ok = tempMapOfMaps["test"].(map[string]interface{})
+		require.True(t, ok)
 	})
-
-	restMapOfMaps, err := convertCtyToInterface(testMapOfMaps)
-
-	tempMapOfMaps, checkMapOfMaps1 := restMapOfMaps.(map[string]interface{})
-	_, checkMapOfMaps2 := tempMapOfMaps["test"].(map[string]interface{})
-	if err != nil || checkMapOfMaps1 != true || checkMapOfMaps2 != true {
-		t.Errorf("Error while converting type maps of maps")
-	}
-
 }
