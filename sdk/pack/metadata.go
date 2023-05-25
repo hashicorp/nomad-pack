3ea10bb5d9196ee7536df39c052c43c1a58302e4
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2023-05-25 16:41:34 +0200

    restore metadata.go

--- sdk/pack/metadata.go
+++ sdk/pack/metadata.go
@@ -100,11 +100,6 @@ func (md *Metadata) ConvertToMapInterface() map[string]interface{} {
 			"description": md.Pack.Description,
 			"version":     md.Pack.Version,
 		},
-		"integration": map[string]interface{}{
-			"identifier": md.Integration.Identifier,
-			"flags":      md.Integration.Flags,
-			"name":       md.Integration.Name,
-		},
 	}
 	if md.Integration != nil {
 		innerMap["integration"] = map[string]interface{}{
